212425722dac7d6e7dc8373976a4b4338cf8e951
Navemics <navemics@gmail.com>
2022-01-19 15:24:18 +0530

    Initial Commit

---
+++ glare/glareApp.swift
@@ -0,0 +1,17 @@
+//
+//  glareApp.swift
+//  glare
+//
+//  Created by NAVEEN MADHAN on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct glareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
